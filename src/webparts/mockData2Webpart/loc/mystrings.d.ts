18dc45cde750f237dd426e04f94f429fcce2013b
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2024-04-22 23:04:30 +0530

    Initial Commit

---
+++ src/webparts/mockData2Webpart/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IMockData2WebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'MockData2WebpartWebPartStrings' {
+  const strings: IMockData2WebpartWebPartStrings;
+  export = strings;
+}
